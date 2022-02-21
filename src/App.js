95d4e9aaa6f1dcbab485887d0bc7ca38d49fac71
rohini <rohiniphatangaren5@gmail.com>
2022-02-21 21:29:54 +0530

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-       
-      </header>
+      <Header />
     </div>
   );
 }
